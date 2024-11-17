6812bde1cd996a6edb4b5b5a4887336b46a50cc4
Kateryna <shkurinakate@gmail.com>
2024-11-17 22:06:49 +0200

    Added method to calculate sum of two numbers

--- myLab8/src/Main.java
+++ myLab8/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Performing 8 laboratory work!");
+
+        int sum = addNumbers(5, 7);
+        System.out.println("5 + 7 = " + sum);
+    }
+
+    public static int addNumbers(int a, int b) {
+        return a + b;
     }
 }
