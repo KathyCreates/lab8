86b010210f4b79714243ad52f5a142c1ac64033a
olhaLK <olga.lashuck@gmail.com>
2024-11-18 20:10:41 +0100

    Square calculation

--- myLab8/src/Main.java
+++ myLab8/src/Main.java
@@ -4,9 +4,14 @@ public class Main {
 
         int sum = addNumbers(5, 7);
         System.out.println("5 + 7 = " + sum);
+
+        int square = squareNumber(6);
+        System.out.println("Square of 6  = " + square);
     }
 
     public static int addNumbers(int a, int b) {
         return a + b;
     }
+
+    public static int squareNumber(int a) { return a * a; }
 }
